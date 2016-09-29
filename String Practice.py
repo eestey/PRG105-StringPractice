b7f36a01e77652b01b626fd034e1b4ebcc12c8f4
eestey <eestey@students.mchenry.edu>
2016-09-29 17:49:53 -0500

    Add files via upload

---
+++ String Practice.py
@@ -0,0 +1,53 @@
+# Print the first letter of the following string
+
+school = "McHenry County College"
+letter = school[0]
+print letter
+
+# print the length of the school string
+school = "McHenry County College"
+len(school)
+print len(school)
+
+# Use a while loop to print each character (including spaces) in the school variable
+index = 0
+while index < len(school):
+    letter = school[index]
+    print letter
+    index += 1
+
+# Slice school into three variables, print the variables
+school = "McHenry County College"
+print school[0:7]
+print school[8:14]
+print school[15:22]
+
+# use a while statement to search for the letter "e" in the contents of the school variable
+# print the index of every location with the letter "e"
+# remember, you should not use the same variable twice in the same program
+# so if you used the variable index, use something else
+
+
+
+
+
+# Write the code to count the number of times the letter y appears in the school variable
+# print the total
+
+word = "McHenry County College"
+count = 0
+for letter in word:
+    if letter == 'y':
+        count += 1
+print count
+
+# create a variable named college and store the upper case version of the variable school in it
+word = "college"
+new_word = word.upper()
+print new_word
+
+# check to see if Count is in the school variable
+
+
+
+# check to see if count is in the school variable
